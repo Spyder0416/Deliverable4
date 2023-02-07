156be50712293aef6d64eed8f18dbeb59815ac2e
Andrew's PC <Andrew's PC@DESKTOP-04A65L1>
2023-02-07 11:38:55 -0500

    Add project files.

---
+++ Deliverable4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace FibonacciSequence
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] fib = new int[26];
+            fib[0] = 0;
+            fib[1] = 1;
+
+            for (int i = 2; i < fib.Length; i++)
+            {
+                fib[i] = fib[i - 2] + fib[i - 1];
+            }
+
+            for (int i = 0; i < fib.Length; i++)
+            {
+                Console.WriteLine("Fibonacci number (" + i + ") = " + fib[i]);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
